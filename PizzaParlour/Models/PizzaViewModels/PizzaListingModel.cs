771ddc8137e0b6533dc423a6204ad2e4afe4a752
Simon Cropper <simon.p.cropper@gmail.com>
2019-12-02 11:47:04 +0000

    View Model binding working

--- PizzaParlour/Models/PizzaViewModels/PizzaListingModel.cs
+++ PizzaParlour/Models/PizzaViewModels/PizzaListingModel.cs
@@ -9,13 +9,16 @@ namespace PizzaParlour.Models.PizzaViewModels
     {
         public Pizza Pizza { get; set; }
         
-        public List<Size> Sizes { get; set; }
+        public IEnumerable<Size> Sizes { get; set; }
+        public int SelectedSizeId { get; set; }
         public IEnumerable<Depth> Depths{ get; set; }
+        public int SelectedDepthId { get; set; }
         public IEnumerable<Topping> Toppings{ get; set; }
+        public List<int> IsToppingChecked { get; set; }
 
         //[DisplayFormat(DataFormatString = "{0:C0}")]
         [DataType(DataType.Currency)]
         public decimal PizzaPrice { get; set; }
-        public string SelectedSize { get; set; }
+        
     }
 }
